22d52079b8be579c46af40b54c09358d6a4f709f
ferryumay <62824271+ferryumay@users.noreply.github.com>
2020-05-06 22:08:04 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-API_HASH = os.getenv("API_HASH")
-API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("1301676")
+API_ID = os.getenv("580488b2ff493e5bf1e6e1cedddfd78a")
 MONGO_URI = os.getenv("MONGO_URI")
 SESSION = os.getenv("SESSION")
